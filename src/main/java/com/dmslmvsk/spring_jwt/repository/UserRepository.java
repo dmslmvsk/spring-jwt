ffb627bcefc0cdf83e75c1b1c6eb93ce93eb2d75
dmslmvsk <dmslmvsk@gmail.com>
2025-03-25 00:45:44 +0100

    basic repositories creation

---
+++ src/main/java/com/dmslmvsk/spring_jwt/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dmslmvsk.spring_jwt.repository;
+
+import com.dmslmvsk.spring_jwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
