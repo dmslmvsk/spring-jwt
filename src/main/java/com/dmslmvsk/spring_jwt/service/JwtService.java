86edf1133bcbfdb996b57cec0dda170dab311cc3
dmslmvsk <dmslmvsk@gmail.com>
2025-03-25 23:31:38 +0100

    created jwt filter

--- src/main/java/com/dmslmvsk/spring_jwt/service/JwtService.java
+++ src/main/java/com/dmslmvsk/spring_jwt/service/JwtService.java
@@ -9,10 +9,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -62,5 +64,44 @@ public class JwtService {
         return parser.build()
                 .parseSignedClaims(token)
                 .getPayload();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver){
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    private boolean isAccessTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isValid(String token, UserDetails user){
+        String username = extractUsername(token);
+
+        boolean isValidToken = tokenRepository.findByAccessToken(token)
+                .map(t -> !t.isLoggedOut()).orElse(false);
+
+        return username.equals(user.getUsername())
+                && isAccessTokenExpired(token)
+                && isValidToken;
+    }
+
+    public boolean isValidRefresh(String token, User user){
+        String username = extractUsername(token);
+
+        boolean isValidRefreshToken = tokenRepository.findByRefreshToken(token)
+                .map(t -> !t.isLoggedOut()).orElse(false);
+
+        return username.equals(user.getUsername())
+                && isAccessTokenExpired(token)
+                && isValidRefreshToken;
     }
 }
